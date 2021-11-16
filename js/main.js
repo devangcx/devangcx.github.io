7f463c5753ca9726b4b86bcbca40e13dbc762e89
Devang <devngc@gmail.com>
2021-11-16 23:53:11 +0530

    fix(main.js): add a function to do selective card styling through JS

--- js/main.js
+++ js/main.js
@@ -52,17 +52,24 @@ function showSlides() {
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
-// Highlight card text if they match certain names
-// document.addEventListener("DOMContentLoaded", function (e) {
-//   const h4s = document.getElementsByClassName("cards");
-//   console.log("Length", h4s.length);
-//   cardsToHighlight = ["Workflow Repository", "Internal Website", "View Script"];
-//   for (i = 0; i < h4s.length; i++) {
-//     console.log("Outer text", h4s[i].lastElementChild.outerText);
-//     if (cardsToHighlight.includes(h4s[i].lastElementChild.outerText)) {
-//       h4s[i].style.cssText =
-//         "text-decoration:none; border-bottom: 5px solid #ee233c;";
-//     }
-//   }
-// });
-// console.log("I am here");
+// Highlight these cards
+const cardsToHighlight = [
+  "workflow repository",
+  "internal website",
+  "view script",
+];
+
+function highlightCards(cards) {
+  document.addEventListener("DOMContentLoaded", function (e) {
+    const h4s = document.getElementsByTagName("h4");
+
+    for (i = 0; i < h4s.length; i++) {
+      if (cardsToHighlight.includes(h4s[i].innerText.toLowerCase())) {
+        bold = h4s[i].children[0];
+        bold.style.cssText =
+          "text-decoration:none; border-bottom: 5px solid #ee233c;";
+      }
+    }
+  });
+}
+highlightCards(cardsToHighlight);
