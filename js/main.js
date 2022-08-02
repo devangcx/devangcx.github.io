fe9c1a5aaea6b3816b70470ed3066038c30daea8
Devang <devngc@gmail.com>
2022-08-01 22:11:28 -0400

    feat(*): separate work navigation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 
-    
-const appBtn = document.getElementById("apps-btn");
 const workEl = document.getElementById("work");
-const workBtn = document.getElementById("work-btn");
 
-appBtn.addEventListener("click", function () {
-    for (let i = 0; i < workEl.children.length; i++) {
-        cardEl = workEl.children[i];
-        if (cardEl.classList.contains("app") === false) {
-            cardEl.style.display = "none";
-        }
-    }
-    document.getElementById("work").scrollIntoView();
-});
+const allBtn = document.getElementById("all-btn");
+const techBtn = document.getElementById("tech-btn");
+const supportBtn = document.getElementById("support-btn");
+const analysisBtn = document.getElementById("analysis-btn");
+const buildingBtn = document.getElementById("buildings-btn");
+const designBtn = document.getElementById("design-btn");
 
-workBtn.addEventListener("click", function () {
+
+
+allBtn.addEventListener("click", function () {
     for (let i = 0; i < workEl.children.length; i++) {
         cardEl = workEl.children[i];
         cardEl.style.display = "flex";
@@ -22,3 +18,85 @@ workBtn.addEventListener("click", function () {
     }
     document.getElementById("work").scrollIntoView();
 });
+
+techBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < workEl.children.length; i++){
+        cardEl = workEl.children[i];
+        cardEl.style.display = "flex";
+    }
+
+    for (let i = 0; i < workEl.children.length; i++) {
+        cardEl = workEl.children[i];
+        if (cardEl.classList.contains("tech") === false && cardEl.classList.contains("work-nav") === false)  {
+            cardEl.style.display = "none";
+        }
+    }
+    document.getElementById("work").scrollIntoView();
+});
+
+supportBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < workEl.children.length; i++){
+        cardEl = workEl.children[i];
+        cardEl.style.display = "flex";
+    }
+
+    for (let i = 0; i < workEl.children.length; i++) {
+        cardEl = workEl.children[i];
+        if (cardEl.classList.contains("support") === false && cardEl.classList.contains("work-nav") === false)  {
+            cardEl.style.display = "none";
+        }
+    }
+    document.getElementById("work").scrollIntoView();
+});
+
+analysisBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < workEl.children.length; i++){
+        cardEl = workEl.children[i];
+        cardEl.style.display = "flex";
+    }
+
+    for (let i = 0; i < workEl.children.length; i++) {
+        cardEl = workEl.children[i];
+        if (cardEl.classList.contains("analysis") === false && cardEl.classList.contains("work-nav") === false)  {
+            cardEl.style.display = "none";
+        }
+    }
+    document.getElementById("work").scrollIntoView();
+});
+
+buildingBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < workEl.children.length; i++){
+        cardEl = workEl.children[i];
+        cardEl.style.display = "flex";
+    }
+
+    for (let i = 0; i < workEl.children.length; i++) {
+        cardEl = workEl.children[i];
+        if (cardEl.classList.contains("building") === false && cardEl.classList.contains("work-nav") === false)  {
+            cardEl.style.display = "none";
+        }
+    }
+    document.getElementById("work").scrollIntoView();
+});
+
+designBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < workEl.children.length; i++){
+        cardEl = workEl.children[i];
+        cardEl.style.display = "flex";
+    }
+
+    for (let i = 0; i < workEl.children.length; i++) {
+        cardEl = workEl.children[i];
+        if (cardEl.classList.contains("design") === false && cardEl.classList.contains("work-nav") === false)  {
+            cardEl.style.display = "none";
+        }
+    }
+    document.getElementById("work").scrollIntoView();
+});
+
+
