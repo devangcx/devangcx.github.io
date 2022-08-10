a9c93cf697d8c3c11f950873b05add5d9774010a
Devang <devngc@gmail.com>
2022-08-10 08:49:30 -0400

    feat(*): inject cards using js

---
+++ js/Card.js
@@ -0,0 +1,23 @@
+
+
+function Card(data) {
+    Object.assign(this, data);
+    const {title, description, link, thumbnail, alt, tag } = this;
+    this.getHtml = function () {
+        return `
+        <div class="card ${tag}">
+            <div class="card-description">
+                <h4>${title}</h4>
+                <p>${description}</p>
+            </div>
+            <a href=${link} target="_blank" class="card-links">
+                <img src=${thumbnail} class="card-img" alt=${alt}>
+            </a>
+        </div>
+        `;
+    };
+}
+
+export default Card;
+
+
---
+++ js/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+    {
+        title: `User manual`,
+        description: `Easy to follow documentation to support industry professionals to develop their own Pollination apps.`,
+        link: `https://docs.pollination.cloud/user-manual/apps/introduction`,
+        thumbnail: `images/thumbnails/user_manual.png`,
+        alt: `link to the Pollination user manual`,
+        tag: 'support'
+    },
+    {
+        title: `Sun-path app`,
+        description: `A web app to see how the sun moves at a certain
+        location on earth and load weather data on it.`,
+        link: `https://sunpath.default.apps.staging.pollination.cloud/`,
+        thumbnail: `images/thumbnails/sunpathIntro.jpg`,
+        alt: `link to the sun path app`,
+        tag: 'tech'
+    },
+    {
+        title: `EPW weather app`,
+        description: `A web app to visualize and interact with the weather data
+        of a certain location for early design analysis by architects and
+        energy modelers.`,
+        link: `https://epw-viz.default.apps.staging.pollination.cloud/`,
+        thumbnail: `images/thumbnails/weather-app.png`,
+        alt: `link to weather report app`,
+        tag: 'tech'
+    }
+];
+
+export default projects;
