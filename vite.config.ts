6598dd0550e2685d7136db74f6541a444f699124
Devang <devngc@gmail.com>
2025-11-09 06:50:04 -0500

    build(vite.config): change port and open in browser

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Open the development server on port 3000 and automatically open in browser
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
