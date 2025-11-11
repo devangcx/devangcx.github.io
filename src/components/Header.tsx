8a50d390323d1478d994fb4e7e6c6ba3be0abb2e
Devang <devngc@gmail.com>
2025-11-11 08:39:38 -0500

    fix: turn name into a link and update footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import "./header.css";
 export default function Header() {
     return (
         <header className="container header">
-            <div className="name">
+            <a href="/" className="name">
                 <h1 className="name-letter indian-saffron">दे</h1>
                 <h1 className="name-letter indian-blue">वां</h1>
                 <h1 className="name-letter indian-green">ग</h1>
-            </div>
+            </a>
             <nav>
                 <a href="/">Blog</a>
                 <a href="/about">About</a>
