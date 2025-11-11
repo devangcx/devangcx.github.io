778b08d67f7620f4d95fa6a13e1d1c08ab49f444
Devang <devngc@gmail.com>
2025-11-11 01:46:37 -0500

    fix: 404 page not found issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   // Open the development server on port 3000 and automatically open in browser
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: "dist",
   },
 })
